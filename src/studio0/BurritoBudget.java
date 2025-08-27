fe11872769fbbd83898982d3f492a39b4c993b1b
NotAlfieW <wyanbing@wustl.edu>
2025-08-27 16:50:38 -0500

    Check Burrito

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -15,6 +15,7 @@ public class BurritoBudget {
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
 		
 		System.out.println("You have spent an average of $" + averageWeeklyPrice + " per week on burritos.");
+		System.out.println("Can you afford to buy 10 burritos in one week? " + (averageWeeklyPrice * 10 < 100));
 	
 	}
 }
